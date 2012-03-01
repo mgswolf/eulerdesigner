4bf5281db2da0eb57d8cfd248dc14e48bf8f0a86
Marcelo G. Silva <mgswolf@gmail.com>
2012-02-29 21:39:34 -0300

    adicionado assets

--- app/mailers/mensageiro.rb
+++ app/mailers/mensageiro.rb
@@ -1,13 +1,13 @@
 # -*- encoding : utf-8 -*-
 class Mensageiro < ActionMailer::Base
   default :from => "from@example.com"
-  
+
 def contato(nome,email,assunto,origem,mensagem)
     @nome = nome
     @email = email
     @assunto=assunto
     @origem=origem
     @mensagem =mensagem
-    mail(:to=>'mgswolf@gmail.com',:from=>nome, :subject=>assunto)
+    mail(:to=>'eulerdesigner@gmail.com',:from=>nome, :subject=>assunto)
   end
 end
